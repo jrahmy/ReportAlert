82d5d1f72f841affe8cec5f9caac5c94beaf589e
Jeremy P <jrahmy+github@gmail.com>
2015-04-10 21:53:24 -0700

    Update release files

--- FileSums.php
+++ FileSums.php
@@ -21,14 +21,14 @@ class FileSums
     /**
      * Provides an associative array of filenames to hashes.
      *
-     * @return array An associative array of filesums.
+     * @return array An associative array of filesums
      */
     public static function getHashes()
     {
         return [
-                'library/Jrahmy/ReportCommentAlert/DataWriter/ReportComment.php' => 'c07d59cc0b933764e011bbbc448f747e',
+                'library/Jrahmy/ReportCommentAlert/DataWriter/ReportComment.php' => '6f2e48a7b84a1a634243c24911c74ba9',
                 'library/Jrahmy/ReportCommentAlert/Listener.php' => 'a818df76120aea84d1c01c8f321b56f9',
-                'library/Jrahmy/ReportCommentAlert/Install.php' => '2a8d44fb0b36e0b55d3e28cd6c75e870',
+                'library/Jrahmy/ReportCommentAlert/Install.php' => 'c255efc31363c810b8e0ea2430951026',
                 'library/Jrahmy/ReportCommentAlert/Model/Report.php' => '46422f9ca1daa6a53e57bf737c1dfba7',
                 'library/Jrahmy/ReportCommentAlert/AlertHandler/Report.php' => 'dfe02acff588a53ce9a5f4ecb034ee7c',
 
