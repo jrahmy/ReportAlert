3cd617d244609cd6b305886fb89549e0e1226d55
Jeremy P <jrahmy+github@gmail.com>
2015-04-12 17:29:32 -0700

    Update comment verbiage

--- Listener.php
+++ Listener.php
@@ -22,7 +22,7 @@ class Listener
      * This method fires when the \XenForo_DataWriter_ReportComment class is
      * loaded.
      *
-     * It extends it with our custom class.
+     * It extends it with a custom class.
      *
      * @param string $class  The class being loaded
      * @param array  $extend An array of classes to extend it with
