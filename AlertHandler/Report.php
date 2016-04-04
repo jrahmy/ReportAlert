ea38a19d7e2c4152d0f2075ffa2ebeb7c1fd2d64
Jeremy P <jrahmy+github@gmail.com>
2016-04-04 03:36:16 -0700

    Wrap AlertHandler method signature to 80 chars

--- AlertHandler/Report.php
+++ AlertHandler/Report.php
@@ -28,8 +28,12 @@ class Report extends \XenForo_AlertHandler_Abstract
      *
      * @return array The content associated with the alerts
      */
-    public function getContentByIds(array $contentIds, $model, $userId, array $viewingUser)
-    {
+    public function getContentByIds(
+        array $contentIds,
+        \XenForo_Model_Alert $model,
+        $userId,
+        array $viewingUser
+    ) {
         /* @var $reportModel \XenForo_Model_Report */
         $reportModel = $model->getModelFromCache('XenForo_Model_Report');
 
